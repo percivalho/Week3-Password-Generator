516dc880cbcfcfd5ded15fdf87ca029f398f7fc3
percivalho <35653644+percivalho@users.noreply.github.com>
2023-04-15 16:47:25 +0800

    5 conditional arguments setup and loop

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 // function generatePassword()
 function generatePassword(){
+  var argLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));
+  while (isNaN(argLength) || argLength < 8 || argLength > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    argLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));          
+  }
+
+  var argIncludeLowercase = confirm("Include lowercase characters? OK to include, Cancel to exclude");
+  var argIncludeUppercase = confirm("Include uppercase characters? OK to include, Cancel to exclude");
+  var argIncludeNumeric = confirm("Include numeric characters? OK to include, Cancel to exclude");
+  var argIncludeSpecial = confirm("Include special characters? OK to include, Cancel to exclude");
+
+
+
+  
+
+
+
   return "abcdef";
 }
 
