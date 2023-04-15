30ceeede6c068a5f2458812e4ca86b7588f64291
percivalho <35653644+percivalho@users.noreply.github.com>
2023-04-15 21:55:28 +0800

    first working solution done

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function getRandomNumber(upbound) {
+  return Math.floor(Math.random() * (upbound + 1));
+}
+
+
 // function generatePassword()
 function generatePassword(){
   var argLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));
@@ -14,13 +21,38 @@ function generatePassword(){
   var argIncludeNumeric = confirm("Include numeric characters? OK to include, Cancel to exclude");
   var argIncludeSpecial = confirm("Include special characters? OK to include, Cancel to exclude");
 
+  if (!argIncludeLowercase && !argIncludeUppercase && !argIncludeNumeric && !argIncludeSpecial) {
+    alert("At least one character type must be selected.");
+    return "";
+  }
 
+  // all conditions have passed
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-=_+[]{}|;':\",./<>?`~\\";
+  var password = "";
+  var masterChars = "";
 
+  if (argIncludeLowercase){
+    masterChars += lowercaseChars;
+  } 
+  if (argIncludeUppercase){
+    masterChars += uppercaseChars;
+  } 
+  if (argIncludeNumeric){
+    masterChars += numericChars;
+  } 
+  if (argIncludeSpecial){
+    masterChars += specialChars;
+  } 
+
+  for (var i = 0; i < argLength; i++) {
+    password += masterChars[getRandomNumber(masterChars.length - 1)];
+  }
   
-
-
-
-  return "abcdef";
+  //return "abcdef";
+  return password;
 }
 
 // Write password to the #password input
