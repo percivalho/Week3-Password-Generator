433727d80518d7e16348d23fcf2ef3740463265f
percivalho <35653644+percivalho@users.noreply.github.com>
2023-04-20 05:06:59 +0100

    add the bonus feature: at least one char for each type

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # Week3-Password-Generator
+
+
+## 03 JavaScript: Password Generator
+
+This week's Challenge requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+
+
+## Usage
+
+open index.html in Google Chrome (recommended) or other internet browser to see the portfolio page.
+
+
+## Features
+
+An effective portfolio highlights your strongest work as well as the thought processes behind it. Students who have portfolios with deployed web applications (meaning they are live on the web) are typically very successful in their career search after the boot camp. This last point can’t be stressed enough: having several deployed projects is a minimum requirement to receive an initial interview at many companies.
+
+With these points in mind, this Challenge allows you to set yourself up for future success by applying the core skills you've recently learned. You'll get to practice your new skills while creating something that you will use during your job search. It’s a win-win that you'll likely be grateful for in the future!
+
+## Tests
+
+Link to Deployed Website:
+https://percivalho.github.io/Week2-Portfolio-Page/
+
+Link to GitHub repo:
+https://github.com/percivalho/Week2-Portfolio-Page.git
+
+
+## License 
+
+![License badge](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+## Credits and Copyright 
+&copy; Copyright 2023 - Present. Percival Ho
--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
+// function to generate the random number from the upper bound given in argument
+// for example: upper bound is 5, it will generate a number from 0-5.
 function getRandomNumber(upbound) {
   return Math.floor(Math.random() * (upbound + 1));
 }
@@ -10,6 +11,7 @@ function getRandomNumber(upbound) {
 
 // function generatePassword()
 function generatePassword(){
+
   var argLength = parseInt(prompt("Enter password length (between 8 and 128 characters):"));
   while (isNaN(argLength) || argLength < 8 || argLength > 128) {
     alert("Password length must be between 8 and 128 characters.");
@@ -33,6 +35,10 @@ function generatePassword(){
   var specialChars = "!@#$%^&*()-=_+[]{}|;':\",./<>?`~\\";
   var password = "";
   var masterChars = "";
+  var lowercaseCharsN = 0;
+  var uppercaseCharsN = 0;
+  var numericCharsN = 0;
+  var specialCharsN = 0;
 
   if (argIncludeLowercase){
     masterChars += lowercaseChars;
@@ -47,10 +53,229 @@ function generatePassword(){
     masterChars += specialChars;
   } 
 
+  // found the number of characters of each tyype in the password generated
   for (var i = 0; i < argLength; i++) {
-    password += masterChars[getRandomNumber(masterChars.length - 1)];
+    char = masterChars[getRandomNumber(masterChars.length - 1)]
+    password += char;
+    if (lowercaseChars.includes(char)){
+      lowercaseCharsN++;
+    }
+    if (uppercaseChars.includes(char)){
+      uppercaseCharsN++;
+    }
+    if (numericChars.includes(char)){
+      numericCharsN++;
+    }
+    if (specialChars.includes(char)){
+      specialCharsN++;
+    }
   }
   
+  // replacement character!
+  // those console logs are debug messages
+  //console.log(lowercaseCharsN);
+  //console.log(uppercaseCharsN);
+  //console.log(numericCharsN);
+  //console.log(specialCharsN);
+  var arrayA = [lowercaseCharsN, uppercaseCharsN, numericCharsN, specialCharsN];
+  //console.log(arrayA);
+  //console.log(Math.max(lowercaseCharsN, uppercaseCharsN, numericCharsN, specialCharsN)); 
+  maxA = Math.max(lowercaseCharsN, uppercaseCharsN, numericCharsN, specialCharsN);
+  //console.log(maxA);
+
+  //var a = password.contains(specialChars);
+  //console.log(a);
+  /*password = "5^ti,vk1";
+  lowercaseCharsN = 4;
+  uppercaseCharsN = 0;
+  numericCharsN = 1;
+  specialCharsN = 3;
+  for (var i = 0; i < password.length; i++) {
+    if (lowercaseChars.indexOf(password[i]) !== -1) {
+      // replace here
+      console.log("herehere!!")
+      console.log(i);
+      var b = uppercaseChars[getRandomNumber(uppercaseChars.length - 1)]
+      console.log(b);
+      //password[i] = b;
+      password = password.slice(0, i) + b + password.slice(i + 1);
+      console.log(password[i]);
+      console.log(password);
+      break;
+    }
+  }    */
+
+  // Replacement logics
+
+  // if lower case needed but no lowercase generated
+  if (argIncludeLowercase && lowercaseCharsN == 0){
+    // replace the character with the most type of characters to replace with lowercase char
+    // if uppercase characters are the maximum
+    if (uppercaseCharsN === maxA){
+      // find the character with uppercaseChars
+      for (let i = 0; i < password.length; i++) {
+        if (uppercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = lowercaseChars[getRandomNumber(lowercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+    } else if (numericCharsN === maxA){
+      // find the character with uppercaseChars
+      for (let i = 0; i < password.length; i++) {
+        if (numericChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = lowercaseChars[getRandomNumber(lowercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    } else if (specialCharsN == maxA) {
+      for (let i = 0; i < password.length; i++) {
+        if (specialChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = lowercaseChars[getRandomNumber(lowercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }
+    }
+  }
+
+  if (argIncludeUppercase && uppercaseCharsN == 0){
+    // replace the character with the most type of character to replace with lowercase char
+
+    if (lowercaseCharsN == maxA){
+      // find the character with uppercaseChars
+      for (let i = 0; i < password.length; i++) {
+        if (lowercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = uppercaseChars[getRandomNumber(uppercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+    } else if (numericCharsN == maxA){
+      // find the character with uppercaseChars
+      for (let i = 0; i < password.length; i++) {
+        if (numericChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = uppercaseChars[getRandomNumber(uppercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+    } else if (specialCharsN == maxA) {
+      for (let i = 0; i < password.length; i++) {
+        if (specialChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = uppercaseChars[getRandomNumber(uppercaseChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    }
+  }
+
+  if (argIncludeNumeric && numericCharsN == 0){
+    // replace the character with the most type of character to replace with lowercase char
+    if (lowercaseCharsN === maxA){
+      for (let i = 0; i < password.length; i++) {
+        if (lowercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = numericChars[getRandomNumber(numericChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    } else if (uppercaseCharsN === maxA){
+      for (let i = 0; i < password.length; i++) {
+        if (uppercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = numericChars[getRandomNumber(numericChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    } else if (specialCharsN == maxA) {
+      for (let i = 0; i < password.length; i++) {
+        if (specialChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = numericChars[getRandomNumber(numericChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    }
+  }
+
+  if (argIncludeSpecial && specialCharsN == 0){
+    // replace the character with the most type of character to replace with lowercase char
+    if (lowercaseCharsN === maxA){
+      // find the character with uppercaseChars
+      for (let i = 0; i < password.length; i++) {
+        if (lowercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = specialChars[getRandomNumber(specialChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    } else if (uppercaseCharsN === maxA){
+      for (let i = 0; i < password.length; i++) {
+        if (uppercaseChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = specialChars[getRandomNumber(specialChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+
+    } else if (numericCharsN == maxA) {
+      for (let i = 0; i < password.length; i++) {
+        if (numericChars.indexOf(password[i]) !== -1) {
+          // replace here
+          var b = specialChars[getRandomNumber(specialChars.length - 1)];
+          password = password.slice(0, i) + b + password.slice(i + 1);
+          break;
+        }
+      }      
+    }
+  }
+
+
+
+  /*var lowercaseCharsN = 0;
+  var uppercaseCharsN = 0;
+  var numericCharsN = 0;
+  var specialCharsN = 0;  
+  for (var i = 0; i < argLength; i++) {
+    if (lowercaseChars.includes(password[i])){
+      lowercaseCharsN++;
+    }
+    if (uppercaseChars.includes(password[i])){
+      uppercaseCharsN++;
+    }
+    if (numericChars.includes(password[i])){
+      numericCharsN++;
+    }
+    if (specialChars.includes(password[i])){
+      specialCharsN++;
+    }
+  }
+  console.log(lowercaseCharsN);
+  console.log(uppercaseCharsN);
+  console.log(numericCharsN);
+  console.log(specialCharsN);*/
+
+
   //return "abcdef";
   return password;
 }
